443d9daf23c667f0509a053f2b991f5966a4707e
Jules Carboni <30209022+Julesc013@users.noreply.github.com>
2020-01-22 22:59:03 +1100

    trying things in a desperate bid to make it work

--- extract_data.py
+++ extract_data.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 from shutil import copyfile
 from openpyxl import Workbook
 from openpyxl import load_workbook,styles
@@ -23,6 +24,7 @@ def search_data(sheet, column, text): # Search the existing data for matches... 
 
 # Define variables
 
+pages_path = r"/home/webscraper/Documents/Newscrape/Pages/"
 results_path = r"/home/webscraper/Documents/Newscrape/Results/"
 all_path = results_path + "all_results.xlsx"
 new_path = results_path + "new_results.xlsx"
@@ -51,29 +53,31 @@ sheet_new = book_new['Sheet1'] # Load the worksheet
 print(" Done.")
 
 
-pages_path = os.fsencode("/home/webscraper/Documents/Newscrape/Pages/") # Get the directory thing
-for page_file in os.listdir(pages_path):
+pages_bytes = os.fsencode(pages_path) # Get the directory thing
+for page_bytes in os.listdir(pages_bytes):
+    page_file = os.fsdecode(page_bytes)
 
-    page_path = os.fsdecode(page_file)
-    print("Parsing " + page_path + "...") # DEBUG
-    parser = html.fromstring(page_path) #page_path.text
+    print("Parsing " + page_file + "...") # DEBUG
+    parser = html.fromstring(page_file) #page_path.text
 
     # Make links absolute
     base_url = "https://www.yellowpages.com.au"
     parser.make_links_absolute(base_url)
 
-    XPATH_LISTINGS = "//div[@class='search-results search-results-data listing-group']" # Get the class containing all the listings from this page
+    XPATH_LISTINGS = "//div[@class='search-results search-results-data listing-group']//div[@class='flow-layout outside-gap-large inside-gap inside-gap-large vertical']//div[conatins(@class, 'cell in-area-cell find-show-more-trial']//div[@class='listing listing-search listing-data']//div[contains(@class, 'search-contact-card')]//div[contains(@class, 'search-contact-card-table-div')]" # Get the class containing all the listings from this page
     listings = parser.xpath(XPATH_LISTINGS)
+
+    print(listings)
 
     for results in listings:
 
         # For each business on this page, gather their data and check if they have already been identified.
 
-        XPATH_BUSINESS_NAME = ".//a[@class='listing-name']//text()" 
+        XPATH_BUSINESS_NAME = ".//table//tbody//tr[@class='search-contact-card-row']//td[@class='search-contact-card-top']//div[contains(@class, 'listing-details')]//div[contains(@class, 'flow-layout')]//div[contains(@class, 'first-cell')]//div[@class='flow-layout outside-gap-large inside-gap inside-gap-medium vertical']//div[@class='cell first-cell last-cell']//div[contains(@class, 'inside-gap-medium vertical')]//div[@class='cell first-cell']//div[contains(@class, 'listing-summary')]//div[@class='body left right']//h2//a[@class='listing-name']//text()" 
         ##XPATH_BUSSINESS_PAGE = ".//a[@class='listing-name']//@href" 
         XPATH_TELEPHONE = ".//a[@class='click-to-call contact contact-preferred contact-phone']//@href"
         XPATH_ADDRESS = ".//a[@class='contact contact-main contact-email']//@data-email"
-        XPATH_LOCATION = ".//p[@class='listing-address mappable-address']//text()"
+        ##XPATH_LOCATION = ".//p[@class='listing-address mappable-address']//text()"
         #XPATH_STREET = ".//div[@class='info']//div//p[@itemprop='address']//span[@itemprop='streetAddress']//text()"
         #XPATH_LOCALITY = ".//div[@class='info']//div//p[@itemprop='address']//span[@itemprop='addressLocality']//text()"
         #XPATH_REGION = ".//div[@class='info']//div//p[@itemprop='address']//span[@itemprop='addressRegion']//text()"
@@ -96,7 +100,7 @@ for page_file in os.listdir(pages_path):
         #raw_zip_code = results.xpath(XPATH_ZIP_CODE)
         #aw_rank = results.xpath(XPATH_RANK)
         raw_address = results.xpath(XPATH_ADDRESS)
-        raw_location = results.xpath(XPATH_LOCATION)
+        ##raw_location = results.xpath(XPATH_LOCATION)
         
         business_name = ''.join(raw_business_name).strip() if raw_business_name else None
         telephone = ''.join(raw_business_telephone).strip() if raw_business_telephone else None
@@ -110,11 +114,11 @@ for page_file in os.listdir(pages_path):
         #region = ''.join(raw_region).strip() if raw_region else None
         #zipcode = ''.join(raw_zip_code).strip() if raw_zip_code else None
         address = ''.join(raw_address).strip() if raw_address else None
-        location = ''.join(raw_location).strip() if raw_location else None
+        ##location = ''.join(raw_location).strip() if raw_location else None
 
 
         # Check if this business listing exists
 
 
         #TEST PRINT
-        print(business_name, telephone, address, location, website)
+        print(business_name + telephone + address + website)
