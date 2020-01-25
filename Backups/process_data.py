61b0e5be44c42dc86ab660a6da48f6c13f8785d9
Jules Carboni <30209022+Julesc013@users.noreply.github.com>
2020-01-25 12:07:38 +1100

    archived old files

---
+++ Backups/process_data.py
@@ -0,0 +1,249 @@
+import os
+import subprocess
+from shutil import copyfile
+from openpyxl import Workbook
+from openpyxl import load_workbook,styles
+import requests
+from lxml import html
+from bs4 import BeautifulSoup
+import unicodecsv as csv
+import argparse
+from requests_testadapter import Resp
+
+class LocalFileAdapter(requests.adapters.HTTPAdapter): # This allows a locally saved HTML file to be retrieved just as the "get" function would.
+    def build_response_from_file(self, request):
+        file_path = request.url[7:]
+        with open(file_path, 'rb') as file:
+            buff = bytearray(os.path.getsize(file_path))
+            file.readinto(buff)
+            resp = Resp(buff)
+            r = self.build_response(request, resp)
+
+            return r
+
+    def send(self, request, stream=False, timeout=None,
+             verify=True, cert=None, proxies=None):
+
+        return self.build_response_from_file(request)
+
+class listing: # Record structure to hold new listings found on each page.
+    def __init__(self, business_name, phone_number, email_address, business_website, yellow_pages_link):
+        self.business_name = business_name
+        self.phone_number = phone_number
+        self.email_address = email_address
+        self.business_website = business_website
+        self.yellow_pages_link = yellow_pages_link
+
+def find_match(sheet, column, text): # Search the existing data for matches... if found, don't count this as a new listing # Sheet must be an Excel worksheet, Column should be in set {A,B,C,D,E} and Text should be alphanumeric. # Returns True if a match is found.
+
+    if text is not None: # Check that the string provided exists
+
+        # Get all cells from specified column
+        for cell in sheet[column]:
+
+            if(cell.value is not None): # We need to check that the cell is not empty
+
+                if cell.value == text: # Check if the value of the cell contains the text
+
+                    return True # return true then break out of this function
+        
+    return False # Else (if none found) return false
+
+
+
+# Define variables
+
+pages_path = r"/home/webscraper/Documents/Newscrape/Pages/"
+results_path = r"/home/webscraper/Documents/Newscrape/Results/"
+all_path = results_path + "all_results.xlsx"
+new_path = results_path + "new_results.xlsx"
+template_path = results_path + "new_results_template.xlsx"
+# Declare list that will hold listing records.
+listings = []
+
+
+# Make a fresh copy of the new-results template for editing.
+
+print("Copying spreadsheet template...", end="")
+
+copyfile(template_path, new_path)
+
+print(" Done.")
+
+
+# Extract the data from each page that was downloaded.
+
+pages_bytes = os.fsencode(pages_path) # Get the directory link
+for page_bytes in os.listdir(pages_bytes):
+
+    # Get file name and path
+    page_file = os.fsdecode(page_bytes)
+    page_path = pages_path + page_file
+
+    print("Getting " + page_file + "...", end="") # DEBUG
+    
+    requests_session = requests.session()
+    requests_session.mount('file://', LocalFileAdapter())
+    response = requests_session.get('file://' + page_path)
+
+    # Check that the page parsed correctly before processing the data.
+    if response.status_code==200:
+
+        print(" Success.")
+        print("Parsing and extracting HTML code... ", end="")
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+        listings_html = soup.find_all("div", attrs={"class": "listing listing-search listing-data"})
+
+        print(" Done.")
+
+
+        # Gather all listings' data and check if they have already been identified.
+
+        for listing_html in listings_html:
+
+            print("Extracting listing " + listing_html.get('data-listing-name') + "..." , end='')
+
+            ## Make links absolute
+            base_url = "https://www.yellowpages.com.au"
+            #listing_html.make_links_absolute(base_url)
+
+
+            # Reset variables
+            business_name_html = None
+            phone_number_html = None
+            email_address_html = None
+            business_website_html = None
+            name = None
+            phone = None
+            email = None
+            website = None
+            yellow_page = None
+
+            # Get the html data of each match.
+            business_name_html = listing_html.find("a", attrs={"class": "listing-name"})
+            #phone_number_html = listing_html.find("a", attrs={"class": "click-to-call contact contact-preferred contact-phone"})
+            phone_number_html = listing_html.find("a", attrs={"class": lambda x: x and 'click-to-call contact contact-preferred' in x})
+            email_address_html = listing_html.find("a", attrs={"class": "contact contact-main contact-email"})
+            business_website_html = listing_html.find("a", attrs={"class": "contact contact-main contact-url"})
+
+
+            # Extract the appropriate sections of each html element.
+
+            # Get sections (only if they exist)
+            if business_name_html is not None:
+                name = business_name_html.text
+            if phone_number_html is not None:
+                phone = phone_number_html.get('href')
+            if email_address_html is not None:
+                email = email_address_html.get('data-email')
+            if business_website_html is not None:
+                website = business_website_html.get('href')
+            if business_name_html is not None:
+                yellow_page = business_name_html.get('href') # Link to the Yellow Pages listing.
+
+            # Add sections to a record
+            record = listing(name, phone, email, website, base_url + yellow_page)
+            #record = listing(name, phone, email, base_url + yellow_page) # Use the YP listing instead of the actual website temporarily.
+            # Append this record to the list of listings
+            listings.append(record)
+
+            print(" Done.")
+
+
+    else:
+        print(" Failed. Error (status) code: " + response.status_code)
+        
+
+# Check if each business listing already exists in our local database
+
+
+# Load worksheets
+
+print("Loading worksheets...", end="")
+
+book_all = load_workbook(filename = all_path) # Load the workbook
+sheet_all = book_all['Sheet1'] # Load the worksheet
+
+book_new = load_workbook(filename = new_path) # Load the workbook
+sheet_new = book_new['Sheet1'] # Load the worksheet
+
+print(" Done.")
+
+
+# Get the last row in the sheet
+final_row_all = sheet_all.max_row
+final_row_new = 1 #sheet_new.max_row # Always start at the top of the sheet
+
+
+# Loop through each newly retrieved record
+
+index = 0
+sheet_index = 0 # This is used so that no rows are skipped in the spreadsheet
+listings_count = len(listings)
+while index <= listings_count - 1:
+
+    business = listings[index]
+
+    print("Checking " + business.business_name + "...", end="")
+
+    this_name = business.business_name
+    this_phone = business.phone_number
+    this_email = business.email_address
+    this_website = business.business_website
+    this_yellow_page = business.yellow_pages_link
+
+    # Search the sheet of all listings to see if it already exists
+    this_name_exists = find_match(sheet_all, "A", this_name)
+    this_phone_exists = find_match(sheet_all, "B", this_phone)
+    this_email_exists = find_match(sheet_all, "C", this_email)
+    this_website_exists = find_match(sheet_all, "D", this_website)
+    this_yellow_page_exists = find_match(sheet_all, "E", this_yellow_page)
+
+    if this_name_exists or this_phone_exists or this_email_exists or this_website_exists or this_yellow_page_exists: # If any of the searches returned a True result for existence
+
+        print(" Already exists.")
+
+        # Keep sheet_index the same
+
+    else:
+
+        print(" Found new listing.")
+
+        # Add the new listing to both spreadsheets
+
+        # Using the old max row as a base get the next row number to write to
+        this_row_all = final_row_all + sheet_index + 1
+        this_row_new = final_row_new + sheet_index + 1 # Start at the top
+
+        print("Adding " + business.business_name + " to spreadsheets...", end="")
+
+        # Add to all listings sheet
+        sheet_all.cell(row=this_row_all, column=1).value = this_name # Name
+        sheet_all.cell(row=this_row_all, column=2).value = this_phone # Phone
+        sheet_all.cell(row=this_row_all, column=3).value = this_email # Email
+        sheet_all.cell(row=this_row_all, column=4).value = this_website # Website
+        sheet_all.cell(row=this_row_all, column=5).value = this_yellow_page # Yellow Page
+
+        # Add to new listings sheet
+        sheet_new.cell(row=this_row_new, column=1).value = this_name # Name
+        sheet_new.cell(row=this_row_new, column=2).value = this_phone # Phone
+        sheet_new.cell(row=this_row_new, column=3).value = this_email # Email
+        sheet_new.cell(row=this_row_new, column=4).value = this_website # Website
+        sheet_new.cell(row=this_row_new, column=5).value = this_yellow_page # Yellow Page
+
+
+        print(" Done.")
+
+        sheet_index += 1
+
+
+    index += 1 # Increment index (b/c not using a for loop)
+
+print("Saving all spreadsheets...", end="")
+
+# Save the changes to the files
+book_all.save(all_path)
+book_new.save(new_path)
+
+print(" Done.")
---
+++ Backups/save_page_as.sh
@@ -0,0 +1,290 @@
+#!/bin/bash
+
+# MODIFIED FOR NEWSCRAPE (required xclip)
+
+set -e
+set -u
+set -o pipefail
+
+# Assert existence of xdotool to begin with
+if ! xdotool --help &>/dev/null; then
+    printf "\e[31mERROR: 'xdotool' is not present (or not in the PATH). Please visit http://www.semicomplete.com/projects/xdotool/ to download it for your platform. \n\e[0m" >&2
+    exit 1
+fi
+
+load_wait_time=4
+save_wait_time=8
+scriptname="$(basename "$0")"
+destination="."
+browser="google-chrome"
+suffix=""
+url=""
+#get_pages_number=false # CUSTOM VARIABLE (USED AT END)
+
+function print_usage() {
+    printf "\e[35m" # Make all magenta
+    printf "\n%s: Open the given url in a browser tab/window, perform 'Save As' operation and close the tab/window.\n\n" "${scriptname}" >&2
+    printf "USAGE:\n  %s URL [OPTIONS]\n\n" "${scriptname}" >&2
+    printf "URL                      The url of the web page to be saved.\n\n" >&2
+    printf "options:\n" >&2
+    printf "  -d, --destination      Destination path. If a directory, then file is saved with default name inside the directory, else assumed to be full path of target file. Default = '%s'\n" "${destination}" >&2
+    printf "  -s, --suffix           An optional suffix string for the target file name (ignored if --destination arg is a full path)\n" >&2
+    printf "  -b, --browser          Browser executable to be used (must be one of 'google-chrome', 'chromium-browser' or 'firefox'). Default = '%s'.\n" "${browser}" >&2
+    printf "  --load-wait-time       Number of seconds to wait for the page to be loaded (i.e., seconds to sleep before Ctrl+S is 'pressed'). Default = %s\n" "${load_wait_time}" >&2
+    printf "  --save-wait-time       Number of seconds to wait for the page to be saved (i.e., seconds to sleep before Ctrl+F4 is 'pressed'). Default = %s\n" "${save_wait_time}" >&2
+    #printf "  --get-pages-number     Scrape the number of pages of listings from the clue url just retrieved and save it to a text file. Default = %s\n" "${get_pages_number}" >&2
+    printf "  -h, --help             Display this help message and exit.\n" >&2
+    printf "\e[0m" # Return to black text
+}
+
+while [ "$#" -gt 0 ]
+do
+    case "$1" in
+        -d | --destination)
+            shift;
+            destination="$1"
+            shift
+            ;;
+        -s | --suffix)
+            shift;
+            suffix="$1"
+            shift;
+            ;;
+        -b | --browser)
+            shift;
+            browser="$1"
+            shift
+            ;;
+
+        --load-wait-time)
+            shift;
+            load_wait_time="$1"
+            shift
+            ;;
+        --save-wait-time)
+            shift;
+            save_wait_time="$1"
+            shift
+            ;;
+        -h | --help)
+            print_usage
+            exit 0
+            ;;
+        #--get-pages-number) # CUSTOM OPTION
+        #    shift;
+        #    get_pages_number=true
+        #    shift
+        #    ;;
+        -*)
+            printf "\e[31mERROR: Unknown option:%s\n\e[0m" "${1}">&2
+            print_usage
+            exit 1
+            ;;
+        *)  if [ ! -z "$url" ]; then
+                printf "\e[31mERROR: Expected exactly one positional argument (URL) to be present, but encountered a second one ('%s').\n\n\e[0m" "${1}" >&2
+                print_usage
+                exit 1
+            fi
+            url="$1"
+            shift;
+            ;;
+    esac
+done
+
+# Returns 1 if input param contains any non-printable or non-ascii character, else returns 0
+# (Inspiration: http://stackoverflow.com/a/13596664/1857518)
+function has_non_printable_or_non_ascii() {
+    LANG=C
+    if printf "%s" "${1}" | grep '[^ -~]\+' &>/dev/null; then
+        printf 1
+    else
+        printf 0
+    fi
+}
+
+function validate_input() {
+    if [[ -z "${url}" ]]; then
+        printf "\e[31mERROR: URL must be specified. \e[0m" >&2
+        print_usage
+        exit 1
+    fi
+
+    if [[ -d "${destination}" ]]; then
+        printf "The specified destination ('%s') is a directory path, will save file inside it with the default name.\n" "${destination}">&2
+    else
+        local basedir="$(dirname "${destination}")"
+        if [[ ! -d "${basedir}" ]]; then
+            printf "\e[31mERROR: Directory '%s' does not exist - Will NOT continue.\n\e[0m" "${basedir}" >&2
+            exit 1
+        fi
+    fi
+    destination="$(readlink -f "$destination")"  # Ensure absolute path
+
+    if [[ "${browser}" != "google-chrome" && "${browser}" != "chromium-browser" && "${browser}" != "firefox" ]]; then
+        printf "\e[31mERROR: Browser (%s) is not supported, must be one of 'google-chrome', 'chromium-browser' or 'firefox'.\n\e[0m" "${browser}" >&2
+        exit 1
+    fi
+
+    if ! command -v "${browser}" &>/dev/null; then
+        printf "\e[31mERROR: Command '${browser}' not found. Make sure it is installed, and in path.\n\e[0m" >&2
+        exit 1
+    fi
+
+    local num_regexp='^.[0-9]+$|^[0-9]+$|^[0-9]+.[0-9]+$'  # Matches a valid number (in decimal notation)
+    if [[ ! "${load_wait_time}" =~ $num_regexp || ! "${save_wait_time}" =~ $num_regexp ]]; then
+        printf "\e[31mERROR: --load-wait-time (='%s'), and --save_wait_time(='%s') must be valid numbers.\n\e[0m" "${load_wait_time}" "${load_wait_time}" >&2
+        exit 1
+    fi
+
+    if [[ $(has_non_printable_or_non_ascii "${destination}") -eq 1 || $(has_non_printable_or_non_ascii "${suffix}") -eq 1 ]]; then
+        printf "\e[31mERROR: Either --destination ('%s') or --suffix ('%s') contains a non ascii or non-printable ascii character(s). " "${destination}" "${suffix}" >&2
+        printf "'xdotool' does not mingle well with non-ascii characters (https://code.google.com/p/semicomplete/issues/detail?id=14).\n\n" >&2
+        printf '\e[1m!!!! Will NOT proceed !!!!\n\e[0m' >&2
+        exit 1
+    fi
+}
+validate_input
+##############
+
+# Launch ${browser}, and wait for the page to load
+"${browser}" "${url}" &>/dev/null &
+sleep ${load_wait_time}
+
+# Find the id for the ${browser} window
+browser_wid="$(xdotool search --sync --onlyvisible --class "${browser}" | head -n 1)"
+wid_re='^[0-9]+$'  # window-id must be a valid integer
+if [[ ! "${browser_wid}" =~ ${wid_re} ]]; then
+    printf "\e[31mERROR: Unable to find X-server window id for browser.\n\e[0m" >&2
+    exit 1
+fi
+
+# Activate the ${browser} window, and "press" ctrl+s
+xdotool windowactivate "${browser_wid}" key --clearmodifiers "ctrl+s"
+
+sleep 1 # Give 'Save as' dialog box time to show up
+
+# Resolve the expected title name for save file dialog box (chrome & firefox differ in this regard)
+if [[ "${browser}" == "firefox" ]]; then
+    savefile_dialog_title="Save as"
+else
+    savefile_dialog_title="Save file"
+fi
+# Find window id for the "Save file" dialog box
+savefile_wid="$(xdotool search --name "$savefile_dialog_title" | head -n 1)"
+if [[ ! "${savefile_wid}" =~ ${wid_re}  ]]; then
+    printf "\e[31mERROR: Unable to find window id for 'Save File' Dialog.\n\e[0m" >&2
+    exit 1
+fi
+
+# Fix for Issue #1: Explicitly focus on the "name" field (works on both: gnome, and kde)
+xdotool windowactivate "${savefile_wid}" key --delay 20 --clearmodifier "Alt+n"
+
+# Check if we are using kde
+is_kde=0
+# Don't feel bad if DESKTOP_SESSION env variable is not present
+set +u
+if [[ "${DESKTOP_SESSION}" =~ ^kde-? ]]; then
+    is_kde=1
+fi
+set -u
+
+if [[ ! -z "${suffix}" ]]; then
+    ###########################
+    # Make sure that we are at correct position before typing the "suffix"
+    #
+    # If the user is using 'kde-plasma', then the full name of the file including the extension is highlighted
+    # in the name field, so simply pressing a Right key and adding suffix leads to incorrect result.
+    # Hence as a special case for 'kde-*' we move back 5 characters Left from the end before adding the suffix.
+    # Now this strategy is certainly not full proof and assumes that file extension is always 4 characters long ('html'),
+    # but this is the only fix I can think for this special case right now. Of course it's easy to tweak the number of
+    # Left key moves you need if you know your file types in advance.
+    if [[ "${is_kde}" -eq 1 ]]; then
+        printf "Desktop session is found to be '${DESKTOP_SESSION}', hence the full file name will be highlighted. " >&2
+        printf "Assuming extension .html to move back 5 character left before adding suffix (change accordingly if you need to).\n" >&2
+        xdotool windowactivate "${savefile_wid}" key --delay 40 --clearmodifier End Left Left Left Left Left
+    else
+        xdotool windowactivate "${savefile_wid}" key --delay 20 --clearmodifiers Right
+    fi
+    set -u
+    ###########################
+
+    extraarg=""
+    if [[ "${suffix::1}}" == "-" ]]; then
+        extraarg="-"
+    fi
+    xdotool type --delay 10 --clearmodifiers "${extraarg}" "${suffix}"
+fi
+
+# Activate the 'Save File' dialog and type in the appropriate filename (depending on ${destination} value: 1) directory, 2) full path, 3) empty)
+if [[ ! -z "${destination}" ]]; then
+    if [[ -d "${destination}" ]]; then
+        # Case 1: --destination was a directory.
+        xdotool windowactivate "${savefile_wid}" key --delay 20 --clearmodifiers Home
+        xdotool type --delay 10 --clearmodifiers "${destination}/"
+    else
+        # Case 2: --destination was full path.
+        xdotool windowactivate "${savefile_wid}" key --delay 20 --clearmodifiers "ctrl+a" "BackSpace"
+        xdotool type --delay 10 --clearmodifiers "${destination}"
+    fi
+fi
+xdotool windowactivate "${savefile_wid}" key --delay 20 --clearmodifiers Return
+
+printf "\e[33mSaving web page...\e[0m" >&2
+
+# Wait for the file to be completely saved
+sleep ${save_wait_time}
+printf "\e[32m Done.\n\e[0m">&2
+
+# Get number of pages of results with this clue
+#if [ "$get_pages_number" = true ]
+#if [ "$(cat ~/Documents/Newscrape/Binaries/pagesnumber.txt)" = "-1" ]
+#then
+#    printf "\e[33mGetting number of results...\e[0m" >&2
+#    sleep 1s #
+#    xdotool key --clearmodifiers F12
+#    sleep 5s #
+#    xdotool key --clearmodifiers Tab
+#    sleep 1s #
+#    xdotool type --delay 50 --clearmodifiers "cell search-message first-cell"
+#    sleep 2s #
+#    xdotool key --clearmodifiers Return
+#    sleep 1s #
+#    xdotool key  --clearmodifiers Tab
+#    sleep 1s #
+#    xdotool key --clearmodifiers Tab
+#    sleep 1s #
+#    xdotool key --clearmodifiers Tab
+#    sleep 1s #
+#    xdotool key --clearmodifiers Right
+#    sleep 1s #
+#    xdotool key --clearmodifiers Down
+#    sleep 1s #
+#    xdotool key --clearmodifiers "ctrl+c"
+
+#    # Process number
+#    resultshtml=$(xclip -out -selection clipboard)
+#    # Get integer substring
+#    resultsnumber=$(echo $resultshtml| cut -d'>' -f 2)
+#    resultsnumber=$(echo $resultsnumber| cut -d' ' -f 1)
+#    # Do the math
+#    if [ "$resultsnumber" -ge 1 -a "$resultsnumber" -le 1000 ]
+#    then
+#        pagesnumber=$(python -c "from math import ceil; print int(ceil($resultsnumber/35.0))")
+#    else
+#        resultsnumber=-1;
+#    fi
+#
+#    # Paste to fresh file
+#    rm ~/Documents/Newscrape/Binaries/pagesnumber.txt
+#    echo $pagesnumber >> ~/Documents/Newscrape/Binaries/pagesnumber.txt
+#    printf "\e[32m Done.\n\e[0m">&2
+#fi
+
+# Close the browser tab/window (Ctrl+w for KDE, Ctrl+F4 otherwise)
+if [[ "${is_kde}" -eq 1 ]]; then
+    xdotool windowactivate "${browser_wid}" key --clearmodifiers "ctrl+w"
+else
+    xdotool windowactivate "${browser_wid}" key --clearmodifiers "ctrl+F4"
+fi
+
+#printf "\e[32mDone!\n\e[0m">&2
