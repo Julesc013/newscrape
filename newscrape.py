79e5c8c157025cd409a071b2c6657a0157ebd0db
Jules Carboni <30209022+Julesc013@users.noreply.github.com>
2020-02-21 07:56:40 +1100

    updated version number

--- newscrape.py
+++ newscrape.py
@@ -91,7 +91,7 @@ def find_match(sheet, column, text): # Search the existing data for matches... i
 # Define variables
 
 # File version information
-version = "1.6.0"
+version = "1.6.1"
 year_copyright = "2020"
 
 # Email details
